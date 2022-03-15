04ebb6a93129cc82746ef1571d93d7f48a7e6295
jcm12 <58841348+jcm12@users.noreply.github.com>
2022-03-15 03:38:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+
+    if (principal <= 0) {
+
+        alert("Enter a positive number.");
+        document.getElementById("prinicpal").focus();
+
+    }
+    else {
+
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var resultToSet = document.getElementById("result");
+
+        var interest = principal * years * rate / 100;
+        
+        var year = new Date().getFullYear() + parseInt(years);
+
+        var toDisplay = `If you deposit <mark>${principal}</mark>,<br/>`;
+        toDisplay += `at an interest rate of <mark>${rate}%</mark>. <br/>`;
+        toDisplay += `You will receive an amount of <mark>${interest}</mark>,<br/>`;
+        toDisplay += `in the year <mark>${year}</mark>.`;
+
+        resultToSet.innerHTML = toDisplay;
+    }
+}
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    var rateToShow = document.getElementById("rate_val");
+
+    rateToShow.innerText = rateval + '%';
     
 }
         
